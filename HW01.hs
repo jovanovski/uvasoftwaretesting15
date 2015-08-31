ddb60dd14e0a1d6cdf4ad926def329598edc1332
Merijn Wijngaard <mwijngaard@gmail.com>
2015-08-31 13:57:41 +0200

    added implementation of lastDigit

--- HW01.hs
+++ HW01.hs
@@ -1,11 +1,13 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW01 where
 
+import Data.Char
+
 -- Exercise 1 -----------------------------------------
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit n = toInteger (digitToInt (last (show n)))
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
