e31a647f8009f5a7afad2709587664292cef7e7c
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-01 10:14:41 +0200

    Added correct problem ordering

--- Private folders/Merijn/opdr1/Euler.hs
+++ Private folders/Merijn/opdr1/Euler.hs
@@ -1,5 +1,13 @@
 module Euler where
 
+tripleswithsum :: Integer -> [(Integer, Integer, Integer)]
+tripleswithsum n = [(a, b, (n - a - b)) | a <- [1..(n `div` 3)-1], b <- [(a+1)..((n-a) `div` 2)]]
+
+sol9 :: [(Integer, Integer, Integer)]
+sol9 = [(a,b,c) | (a,b,c) <- tripleswithsum 1000, a^2 + b^2 == c^2]
+
+
+
 -- Continuing to sieve after x^2 > n takes too much time and is not necessary because we only use primes up to n.
 -- After x^2 > n all remaining numbers between x and n must be prime, otherwise they would have already cancelled 
 -- out by an earlier sieve step since they would've been a multiple of a number smaller than x. 
@@ -9,10 +17,3 @@ primesbelow n = sievebelow [2..] where
 
 sol10 :: Integer
 sol10 = sum (primesbelow 2000000)
-
-
-tripleswithsum :: Integer -> [(Integer, Integer, Integer)]
-tripleswithsum n = [(a, b, (n - a - b)) | a <- [1..(n `div` 3)-1], b <- [(a+1)..((n-a) `div` 2)]]
-
-sol9 :: [(Integer, Integer, Integer)]
-sol9 = [(a,b,c) | (a,b,c) <- tripleswithsum 1000, a^2 + b^2 == c^2]
