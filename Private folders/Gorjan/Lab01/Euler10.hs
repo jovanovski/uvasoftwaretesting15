3951ec9b465c6f7e7f9532bc8197b57ffe86e2ac
Gorjan Jovanovski <jovanovski@gorjan.info>
2015-09-01 17:53:05 +0200

    Initial Euler49 commit

--- Private folders/Gorjan/Lab01/Euler10.hs
+++ Private folders/Gorjan/Lab01/Euler10.hs
@@ -15,7 +15,7 @@ sieve (n : xs) = n : sieve (mark xs 1 n)
 		mark (y:ys) k m | k == m = 0 : (mark ys 1 m)
 						| otherwise = y : (mark ys (k+1) m)
 
-
-primes = sum(sieve [2..2000000])
+primesSum :: Integer
+primesSum = sum(sieve [2..200])
 
 
