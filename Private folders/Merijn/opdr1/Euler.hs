b3e9c2fd33d73c1aa3d5333a7d38855ef36b08ef
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-01 09:51:06 +0200

    added comment explaining primesbelow function

--- Private folders/Merijn/opdr1/Euler.hs
+++ Private folders/Merijn/opdr1/Euler.hs
@@ -1,8 +1,12 @@
 module Euler where
 
+-- Continuing to sieve after x^2 > n takes too much time and is not necessary because we only use primes up to n.
+-- After x^2 > n all remaining numbers between x and n must be prime, otherwise they would have already cancelled 
+-- out by an earlier sieve step since they would've been a multiple of a number smaller than x. 
 primesbelow :: Integer -> [Integer]
 primesbelow n = sievebelow [2..] where 
   sievebelow (x:xs) = if x^2 > n then x:takeWhile (< n) xs else x:(sievebelow [y|y <- xs, y `mod` x /= 0])
 
 sol10 :: Integer
 sol10 = sum (primesbelow 2000000)
+
