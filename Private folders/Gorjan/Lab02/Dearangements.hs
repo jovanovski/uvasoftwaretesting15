f96c7cf1e32dab0bd716229710d188f899df936b
Gorjan Jovanovski <jovanovski@gorjan.info>
2015-09-08 13:31:56 +0200

    IBAN complete

--- Private folders/Gorjan/Lab02/Dearangements.hs
+++ Private folders/Gorjan/Lab02/Dearangements.hs
@@ -5,9 +5,26 @@ isDerangement :: [Integer] -> [Integer] -> Bool
 isDerangement [] [] = True
 isDerangement (x:xs) (y:ys) = if x/=y then isDerangement xs ys
 								else False
+isDerangement2 :: [Integer] -> [Integer] -> Bool
+isDerangement2 [] [] = True
+isDerangement2 (x:xs) (y:ys) = True --TODO
+
+---
+
+checkLength :: [Integer] -> [Integer] -> Bool
+checkLength [] [] = True
+checkLength xs ys = length xs == length ys
+
+checkElems :: [Integer] -> [Integer] -> Bool
+checkElems [] [] = True
+checkElems xs ys = sort xs == sort ys
+
+---
 
 deran :: Integer -> [[Integer]]
 deran x = filter (\y -> isDerangement y [0..(x-1)]) (permutations [0..(x-1)])
+
+
 
 --time needed : 5 minutes
 --TODO tests
