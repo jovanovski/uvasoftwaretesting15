aef12bcb0c40156d160a0c5ecee432831f7f8062
Gorjan Jovanovski <jovanovski@gorjan.info>
2015-09-08 13:56:30 +0200

    Derangements done

--- Private folders/Gorjan/Lab02/Dearangements.hs
+++ Private folders/Gorjan/Lab02/Dearangements.hs
@@ -3,28 +3,28 @@ import Data.List
 
 isDerangement :: [Integer] -> [Integer] -> Bool
 isDerangement [] [] = True
-isDerangement (x:xs) (y:ys) = if x/=y then isDerangement xs ys
-								else False
-isDerangement2 :: [Integer] -> [Integer] -> Bool
-isDerangement2 [] [] = True
-isDerangement2 (x:xs) (y:ys) = True --TODO
+isDerangement (x:xs) (y:ys) = if x/=y then isDerangement xs ys else False
 
----
 
-checkLength :: [Integer] -> [Integer] -> Bool
-checkLength [] [] = True
-checkLength xs ys = length xs == length ys
+-- fucntion extracted from the Rosetta Code (http://rosettacode.org/wiki/Permutations/Derangements#Haskell), presumed to be functional
+derangements :: [Integer] -> [[Integer]]
+derangements xs = filter (and . zipWith (/=) xs) $ permutations xs
 
-checkElems :: [Integer] -> [Integer] -> Bool
-checkElems [] [] = True
-checkElems xs ys = sort xs == sort ys
+--- precondition tests
+
+checkInput :: Integer -> Bool
+checkInput n = n>=0
 
 ---
 
 deran :: Integer -> [[Integer]]
-deran x = filter (\y -> isDerangement y [0..(x-1)]) (permutations [0..(x-1)])
+deran x = if checkInput x then filter (\y -> isDerangement y [0..(x-1)]) (permutations [0..(x-1)]) else error "Bad Input (<0)"
 
+testFunctions :: Integer -> Bool
+testFunctions x = deran x == derangements [0..(x-1)] 
 
+runTests :: Integer -> Integer -> Integer -> String
+runTests 0 p f = "Passed " ++ (show p) ++ " tests, failed " ++ (show f) ++ " tests"
+runTests n p f = if testFunctions n then runTests (n-1) (p+1) f else runTests (n-1) p (f+1)
 
---time needed : 5 minutes
---TODO tests
+--time needed : 15 minutes
