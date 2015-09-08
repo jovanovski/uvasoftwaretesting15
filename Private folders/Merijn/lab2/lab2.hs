f445eef465a1a50453543cac803b2cb953e10ea5
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-08 21:09:10 +0200

    Added comments relating to exercises and removed some paretheses

--- Private folders/Merijn/lab2/lab2.hs
+++ Private folders/Merijn/lab2/lab2.hs
@@ -5,6 +5,8 @@ import Data.Char
 import System.Random
 import Testing
 
+-- START `Recognizing triangles`
+
 data Shape = NoTriangle | Equilateral | Rectangular | Isosceles | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
@@ -42,6 +44,12 @@ triangleTests = [ Test "triangle test" testTriangle
     ]
   ]
 
+-- END `Recognizing triangles`
+-- 
+-- Time taken: 30m
+
+-- START `Recognizing permutations` 
+
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation [] [] = True
 isPermutation [] _ = False
@@ -76,7 +84,7 @@ genIntList m n = do
     genIntListL r = do
       t <- getRandomInt n
       ts <- genIntListL (r-1)
-      return (t:ts)
+      return $ t:ts
 
 -- m = number of lists to generate, n = max length of list, p = max bound of int
 genPermTestCases :: Int -> Int -> Int -> IO [([Int], [Int], Bool)]
@@ -86,29 +94,19 @@ genPermTestCases m n p = do
   s <- (shuffle r)
   t <- genPermTestCases (m-1) n p
   u <- getRandomInt 1
-  if length r == 0 || u == 0 then return ((r, s, True) : t)
+  if length r == 0 || u == 0 then 
+    return $ (r, s, True) : t
   else 
     do 
       v <- getRandomInt ((length s)-1)
-      return ((r, (take v s) ++ [(s!!v)+1] ++ (drop (v+1) s), False) : t)
+      return $ (r, take v s ++ [(s!!v)+1] ++ drop (v+1) s, False) : t
 
 shuffle :: Eq a => [a] -> IO [a]
 shuffle [] = return []
 shuffle xs = do
   p <- getRandomInt (length xs - 1)
-  q <- shuffle (delete (xs !! p) xs)
-  return ((xs !! p) : q)
-
-changeAtRandom :: Eq a => [a] -> IO [a]
-changeAtRandom [] = return []
-changeAtRandom ys@(x:xs) = do
-  p <- getRandomInt 1
-  q <- getRandomInt (length xs)
-  if p == 0 then 
-    return ys
-  else
-    let (a,b) = splitAt q xs
-    in return (a ++ [x] ++ b)
+  q <- shuffle $ delete (xs !! p) xs
+  return $ (xs !! p) : q
 
 autoTestIsPermutation :: IO ()
 autoTestIsPermutation = do
@@ -119,11 +117,22 @@ autoTestIsPermutation = do
       let msg  = show a ++ " is " ++ (if r then "" else "not ") ++ "a permutation of " ++ show b
       in  if isPermutation a b == r then
             do
-              print ("pass on: " ++ msg)
+              print $ "pass on: " ++ msg
               helper ys
           else
             do 
-              print ("failed on: " ++ msg)
+              print $ "failed on: " ++ msg
+
+-- END `Recognizing permutations`
+-- 
+-- Automating test process: For correct cases this is easily possible using the shuffle function, but to fully
+-- test the function we would also need to create deviations. This can be done using the method shown in
+-- the genPermTestCases function, but it only creates deviations with a single modified element and of the same
+-- length as the original. To implement automated tests for all possible deviations would be a lot of work.
+-- 
+-- time taken: 2,5h
+
+-- START `Recognizing and generating derangements`
 
 isDerangement :: Eq a => [a] -> [a] -> Bool
 isDerangement [] [] = True
@@ -133,10 +142,10 @@ isDerangement (x:xs) (y:ys) = x /= y && isDerangement xs ys
 
 perms :: Eq a => [a] -> [[a]]
 perms [] = [[]]
-perms xs = [ y:z | y <- xs, z <- perms (delete y xs)]
+perms xs = [ y:z | y <- xs, z <- perms $ delete y xs]
 
 deran :: Eq a => [a] -> [[a]]
-deran xs = [y | y <- perms xs, (isDerangement xs y) == True]
+deran xs = [y | y <- perms xs, isDerangement xs y == True]
 
 testIsDerangement :: Eq a => ([a], [a], Bool) -> Bool
 testIsDerangement (a,b,r) = isDerangement a b == r
@@ -154,6 +163,17 @@ isDerangementTests = [Test "is derangement test" testIsDerangement
       ([1,2], [], False)
     ]
   ]
+
+-- END Recognizing and generating derangements`
+--
+-- Automating test process: It is difficult to automate this test process in a useful way because generating 
+-- derangements uses the isDerangement function internally. Of course this could be rewritten to not use the 
+-- isDerangement function itself but only it's internal functionality, but this would not change much about 
+-- the usefulness.
+-- 
+-- time taken: 15m
+
+-- START `Implementing and testing IBAN validation` 
 
 iban :: String -> Bool
 iban s = ibanCheckDigits s == 1
@@ -198,19 +218,19 @@ genIbanTestCases n m = do
   r <- genIbanTestCases (n-1) m
   let cd = 98 - ibanCheckDigits (cc ++ "00" ++ bban)
       cds = if cd < 10 then "0" ++ show cd else show cd 
-  return ((cc ++ cds ++ bban, True) : r)
+  return $ (cc ++ cds ++ bban, True) : r
 
 genAlphanumericList :: Int -> IO String
 genAlphanumericList 0 = return ""
 genAlphanumericList n = do
   o <- getRandomAlphanumeric 
   p <- genAlphanumericList (n-1)
-  return (o : p)
+  return $ o : p
 
 getRandomAlphanumeric :: IO Char
 getRandomAlphanumeric = do
   x <- getRandomInt 35
-  return (if x < 10 then intToDigit x else chr (x + 55))
+  return $ if x < 10 then intToDigit x else chr (x + 55)
 
 autoTestIban :: IO ()
 autoTestIban = do
@@ -221,12 +241,18 @@ autoTestIban = do
       let msg = show a ++ " is " ++ (if r then "" else "not ") ++ "a valid iban"
       in  if iban a == r then
             do
-              print ("pass on: " ++ msg)
+              print $ "pass on: " ++ msg
               helper ys
           else
             do 
-              print ("failed on: " ++ msg)
+              print $ "failed on: " ++ msg
 
+-- END `Implementing and testing IBAN validation` 
+--
+-- Automating test process: As shown, generating random ibans internally uses the same functionality as the
+-- iban check itself. Just like for the derangements, this is not very useful. 
+-- 
+-- time taken: 1,5h
 
 allTests = concat [
     triangleTests,
