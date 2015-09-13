9eb269bbd817afa7e4042182e8b9d7751b3e1c03
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-13 20:32:29 +0200

    added bonus euler answers to group folder

---
+++ Lab02/Euler_Problem20_fac.hs
@@ -0,0 +1,13 @@
+import Data.Char
+fact :: Integer -> Integer
+fact 0 = 1
+fact n = (fact' (n-1))*n
+
+-- works only with numbers 0-9
+sumOfDigits :: String -> Int
+sumOfDigits "" = 0
+sumOfDigits (x:xs) = sumOfDigits xs + ((ord x) - 48)
+
+problem20 :: Int
+problem20 = sumOfDigits $ show (fact 100)
+	
