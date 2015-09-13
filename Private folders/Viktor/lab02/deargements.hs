5723f980d259b6f0cda3acf5f2a406ee44f2c056
sklencar <vsklencar@gmail.com>
2015-09-13 18:23:50 +0200

    Lab02
    Mostly as previous version, only tests were modified to more official
    version. Had some problem with showing test.

--- Private folders/Viktor/lab02/deargements.hs
+++ Private folders/Viktor/lab02/deargements.hs
@@ -1,5 +1,8 @@
+import Data.List
+import Testing
+
 -- Ex 03
-isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement :: [Integer] -> [Integer] -> Bool
 isDerangement [] [] = True
 isDerangement [] _ = True
 isDerangement _ [] = True
@@ -13,12 +16,44 @@ perms (x:xs) = concat (map (insrt x) (perms xs)) where
  	  		insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
 
 
-deran :: Int -> [[Int]]
---deran n =  [0..n-1]
-deran n = [x  | x <- perms $ createList n, isDerangement x]
+deran :: Integer -> [[Integer]]
+deran 0 =  []
+deran n = filter (\y -> isDerangement y [0..(n-1)]) (perms [0..(n-1)])
 
-createList :: Int -> [Int]
+createList :: Integer -> [Integer]
 createList n = [0..n-1]
 
 
---testing
+--TESTING
+checkLength :: [Integer] -> [Integer] -> Bool
+checkLength xs ys 	| length xs == length ys = True
+					| otherwise = False
+
+checkChars :: [Integer] -> [Integer] -> Bool
+checkChars xs ys = if sort xs == sort ys then True else False
+
+derangements :: [Integer] -> [[Integer]]
+derangements xs = filter (and . zipWith (/=) xs) $ permutations xs
+
+testGen :: Integer -> Bool
+testGen n = if sort (deran n) == sort (derangements [0..(n-1)]) then True else False
+
+assertTrue :: Bool -> Bool
+assertTrue b = b == True
+
+test = test' 3
+
+test' 0 = return ()
+test' n = do
+		let res = testGen n
+		print("Test for length "++(show n)++(": ")++(show res))
+		test' (n-1)
+
+{--
+-- It takes parameter n and uses it as a length information for generating derangements
+-- estimation time:1,5h , real time: 3h
+test
+"Test for length 3: True"
+"Test for length 2: True"
+"Test for length 1: True"
+--}
--- Private folders/Viktor/lab02/isPermutation.hs
+++ Private folders/Viktor/lab02/isPermutation.hs
@@ -1,3 +1,6 @@
+import Data.List
+import System.Random
+import Testing
 -- Ex 02
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation [] [] = True
@@ -5,3 +8,92 @@ isPermutation xs [] = False
 isPermutation [] _ = False
 isPermutation (x:xs) (ys) | x `elem` ys = isPermutation xs (delete x ys)
 						  | otherwise = False
+
+
+-- TESTING
+perms :: [a] ->[[a]]
+perms [] = [[]]
+perms (x:xs) = concat (map (insrt x) (perms xs)) where
+		   	insrt x [] = [[x]]
+ 	  		insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
+
+
+
+
+isPermutation2 :: Eq a => [a] -> [a] -> Bool
+isPermutation2 xs ys = isContained ys (perms xs)
+
+getRandomInt :: Int -> Int -> IO Int
+getRandomInt l h = getStdRandom (randomR (l,h))
+
+isContained :: Eq a => [a] -> [[a]] -> Bool
+isContained _ [] = False
+isContained xs (ys:yss)  | xs == ys = True
+						 | otherwise = isContained xs yss
+
+
+-- randomly pick two of given list, return a pair
+pickTwo :: [[a]] -> IO ([a],[a])
+pickTwo xss = do
+		i <- getRandomInt 0 ((length xss)-1)  
+		j <- getRandomInt 0 ((length xss)-1) -- might happend i == j
+		return (xss!!i,xss!!j)
+
+
+randomFlip :: Int -> IO Int
+randomFlip x = do 
+   b <- getRandomInt 0 1
+   if b==0 then return x else return (-x)
+
+genIntList :: IO [Int]
+genIntList = do 
+  k <- getRandomInt 0 20
+  n <- getRandomInt 0 10
+  getIntL k n
+
+getIntL :: Int -> Int -> IO [Int]
+getIntL _ 0 = return []
+getIntL k n = do 
+   x <-  getRandomInt 0 k
+   y <- randomFlip x
+   xs <- getIntL k (n-1)
+   return (y:xs)
+
+
+testIsPerm :: IO String
+testIsPerm = do
+	list <- genIntList
+	(xs,ys) <- pickTwo (perms list)
+	print ("Selected lists: "++(show xs)++ " and "++(show ys))
+	let r1 = isPermutation xs ys
+	print("isPermutation: "++(show r1))
+	let r2 = isPermutation2 xs ys
+	print("isPermutation2: "++(show r2))
+	let r = r1 ==r2
+	let res = r == True
+	let output = ("Result: "++(show res))
+	print (output)
+	return output
+
+-- runs testIsPerm 3 times (put there your number)
+test = test' 3
+
+test' 0 = return ()
+test' n = do
+		testIsPerm
+		test' (n-1)
+{--REPORT
+-- estimation time 1h, real time 3h (always more because of the tests)
+"Selected lists: [-10,0,-5,15,-2] and [-2,-5,-10,15,0]"
+"isPermutation: True"
+"isPermutation2: True"
+"Result: True"
+"Selected lists: [4,16,4,-11,-6,-17,-8,-11] and [-6,4,-17,-11,4,-8,16,-11]"
+"isPermutation: True"
+"isPermutation2: True"
+"Result: True"
+"Selected lists: [-6,13,3,-8,-7,11] and [3,11,-8,13,-7,-6]"
+"isPermutation: True"
+"isPermutation2: True"
+"Result: True"
+--}
