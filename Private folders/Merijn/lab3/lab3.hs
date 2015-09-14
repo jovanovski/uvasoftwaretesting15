b0c36f07bd08b224beeff97b95ba49114be0c1cf
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-14 13:59:19 +0200

    Added tests for part 1

--- Private folders/Merijn/lab3/lab3.hs
+++ Private folders/Merijn/lab3/lab3.hs
@@ -3,20 +3,77 @@ module Lab3 where
 import Data.List
 import System.Random
 import Lecture3
+import Testing
 import Test.QuickCheck
 
 contradiction :: Form -> Bool
 contradiction f = not (any (\v -> evl v f) (allVals f))
 
+testContradiction :: (Form, Bool) -> Bool
+testContradiction (f,e) = contradiction f == e
+
+contradictionTests :: [Test]
+contradictionTests = [ Test "contradiction tests" testContradiction
+    [
+      (Cnj [Prop 1, Neg (Prop 1)], True),
+      (Cnj [Dsj [Prop 1, Prop 2], Cnj [Neg (Prop 1), Neg (Prop 2)]], True),
+      (Cnj [Prop 1, Prop 2], False)
+    ]
+  ]
+
 tautology :: Form -> Bool
 tautology f = all (\v -> evl v f) (allVals f)
 
+testTautology :: (Form, Bool) -> Bool
+testTautology (f,e) = tautology f == e
+
+tautologyTests :: [Test]
+tautologyTests = [ Test "tautology tests" testTautology
+    [
+      (Dsj [Prop 1, Neg (Prop 1)], True),
+      (Cnj [Dsj [Prop 1, Neg (Prop 1) ], Dsj [Prop 2, Neg (Prop 2)] ], True),
+      (Cnj [Prop 1, Prop 2], False)
+    ]
+  ]
+
 allVals2 :: Form -> Form -> [Valuation]
-allVals2 f g = nub ((allVals f) ++ (allVals g))
+allVals2 f g = nub [sort (nub (vf ++ vg)) | vf <- allVals f, vg <- allVals g]
 
 entails :: Form -> Form -> Bool
 entails f g = all (\v -> not (evl v f) || evl v g) (allVals2 f g)
 
+testEntails :: (Form, Form, Bool) -> Bool
+testEntails (f,g,e) = entails f g == e
+
+entailsTests :: [Test]
+entailsTests = [ Test "entails tests" testEntails
+    [
+      (Prop 1, Prop 1, True),
+      (Cnj [Prop 1, Prop 2], (Impl (Prop 1) (Prop 2)), True),
+      ((Impl (Prop 1) (Prop 2)), Cnj [Prop 1, Prop 2], False),
+      (Prop 1, Neg (Prop 1), False)
+    ]
+  ]
+
 equiv :: Form -> Form -> Bool
 equiv f g = all (\v -> evl v f == evl v g) (allVals2 f g)
 
+testEquiv :: (Form, Form, Bool) -> Bool
+testEquiv (f,g,e) = equiv f g == e
+
+equivTests :: [Test]
+equivTests = [ Test "equiv tests" testEquiv
+    [
+      (Prop 1, Prop 1, True),
+      (Impl (Prop 1) (Prop 2), Dsj [Neg (Prop 1), Prop 2], True),
+      (Prop 1, Prop 2, False),
+      (Cnj [Prop 1, Prop 2], Dsj [Prop 1, Prop 2], False)
+    ]
+  ]
+
+allTests = concat [
+    contradictionTests,
+    tautologyTests,
+    entailsTests,
+    equivTests
+  ]
