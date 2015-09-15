ace454a9d0afcb6ba463ab5e41182aeeca120692
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-15 12:11:53 +0200

    Added test report for parse function

--- Private folders/Merijn/lab3/lab3.hs
+++ Private folders/Merijn/lab3/lab3.hs
@@ -97,14 +97,28 @@ testParse (s,e) = show (parse s) == e
 parseTests :: [Test]
 parseTests = [ Test "parse tests" testParse
     [
+      ("1", "[1]"),
+      ("(1)", "[1]"),
+      ("*(1 2)", "[*(1 2)]"),
+      ("*((1 2))", "[*(1 2)]"),
       ("*(1 +(2 -3))", "[*(1 +(2 -3))]"),
       ("*(1 +(2 -3)", "[]"),
-      ("*(1 +(2 -3))))", "[*(1 +(2 -3))]")
+      ("*(1 +(2 -3))))", "[*(1 +(2 -3))]"),
+      ("+(1 2)", "[+(1 2)]"),
+      ("1==>2", "[1==>2]"),
+      ("(1==>2)", "[1==>2]"),
+      ("((1==>2))", "[1==>2]"),
+      ("1<=>2", "[1<=>2]"),
+      ("(1<=>2)", "[1<=>2]"),
+      ("((1<=>2))", "[1<=>2]")
     ]
   ]
 
 -- END test parse 
--- Time spent: 30m, tested using `runTests parseTests`
+-- Time spent: 1h, tested using `runTests parseTests`. 
+-- Test result are not so good:  
+--    * Implications and equivalences only work in parentheses
+--    * Multiple parentheses are not supported
 
 -- START convert to CNF
 
