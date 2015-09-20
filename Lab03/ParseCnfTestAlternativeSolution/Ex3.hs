25f5f2701fd8f234b4a6ebf5a6fc2627f11486bb
Gorjan Jovanovski <jovanovski@gorjan.info>
2015-09-20 20:01:17 +0200

    Added parse/cnf test with alternative solution

---
+++ Lab03/ParseCnfTestAlternativeSolution/Ex3.hs
@@ -0,0 +1,40 @@
+module Ex3 where
+
+import Lecture3
+import Test.QuickCheck
+
+cnf :: String -> Form
+cnf xs = Cnj (cnfForm(flipVals (falseVals (allVals(parse xs!!0)) (parse xs!!0))))
+
+cnfForm :: [Valuation] -> [Form]
+cnfForm [] = []
+cnfForm (x:xs) = getDsj(mDOV x) : cnfForm xs
+
+falseVals :: [Valuation] -> Form -> [Valuation]
+falseVals [] _ = []
+falseVals (x:xs) f = if (evl x f) then falseVals xs f else x: falseVals xs f
+
+flipVals :: [Valuation] -> [Valuation]
+flipVals [] = []
+flipVals (x:xs) = flipVal x : flipVals xs
+
+flipVal :: Valuation -> Valuation
+flipVal [] = []
+flipVal ((i, b):xs) = (i, not b) : flipVal xs
+
+getDsj :: [Form] -> Form
+getDsj xs = Dsj xs
+
+mDOV :: Valuation -> [Form]
+mDOV [] = []
+mDOV ((i, b):xs) = if b then (Prop i) : mDOV xs else Neg (Prop i) :mDOV xs
+
+-- | Time spent: 30min
+
+-- | Steps taken:
+-- | take forumula
+-- | parse formula
+-- | find truth values
+-- | find pairs for which eval is false
+-- | create NOT p or NOT q or NOT r ...
+-- | create conjuction of them
