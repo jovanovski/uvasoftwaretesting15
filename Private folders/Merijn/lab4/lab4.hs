5811a663528e66f7c8ad398f8023561938e4d456
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-21 17:29:30 +0200

    Added answer for part 7

--- Private folders/Merijn/lab4/lab4.hs
+++ Private folders/Merijn/lab4/lab4.hs
@@ -136,4 +136,26 @@ prop_TrClosContainsOriginal r = let c = trClos r in all (\x -> x `elem` c) r
 -- END Testing symClos & trClos
 -- Time taken 20m - tested uing quickcheck
 
--- START 
+-- START sym tr clos is tr sym clos 
+
+prop_SymTrClosIsTrSymClos :: Rel Int -> Bool
+prop_SymTrClosIsTrSymClos r = let stc = symClos $ trClos r; tsc = trClos $ symClos r in stc == tsc
+
+{-
+They are NOT the same - the transitive closure of a symmetric closure is also reflexive, while a symmetric 
+closure of a transitive closure does not need to be so. An example would be: 
+
+  [(1,0)]
+
+When first taking the transitive closure and then the symmetric closure, this would lead to:
+
+  [(1,0)] -> [(1,0)] -> [(0,1),(1,0)]
+
+While first taking the symmetric closure and then the transitive closure leads to:
+
+  [(1,0)] -> [(0,1),(1,0)] -> [(0,0),(0,1),(1,0),(1,1)]
+-}
+
+-- END sym tr clos is tr sym clos
+-- Time taken 30m 
+
