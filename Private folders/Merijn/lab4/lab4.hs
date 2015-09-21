71b02ba6e15220c3d07c611ea43807b26e6edfdf
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-21 14:50:31 +0200

    Added tests using own generator for set functions

--- Private folders/Merijn/lab4/lab4.hs
+++ Private folders/Merijn/lab4/lab4.hs
@@ -10,30 +10,81 @@ import Data.List
 -- START random set generator
 
 -- n = number of test cases to generate
-genIntSets :: Int -> IO ([Set Int])
-genIntSets 0 = return []
-genIntSets n = do
-  l <- genIntList 20 20
-  r <- genIntSets (n-1)  
-  return $ (list2set l) : r
+genIntSet :: IO (Set Int)
+genIntSet = do
+  l <- genIntList 15 15  
+  return $ list2set l
 
 instance (Ord a, Arbitrary a) => Arbitrary (Set a) where 
-  arbitrary = liftM list2set arbitrary
+  arbitrary = do
+    a <- arbitrary
+    return $ list2set a
 
 -- END random set generator 
 -- Time taken 30m, tested using `genIntSet 50 50` and `sample $ (arbitrary :: Gen (Set Int))`
 
 -- START set operations
 
--- union already implemented in SetOrd
-prop_UnionContainsAllElements :: Set Int -> Set Int -> Bool
-prop_UnionContainsAllElements f@(Set fs) g@(Set gs) = 
+-- unionSet already implemented in SetOrd
+
+testSetProp2 :: (Set Int -> Set Int -> Bool) -> Int -> IO()
+testSetProp2 _ 0 = print "All passed"
+testSetProp2 p n = do
+  f <- genIntSet
+  g <- genIntSet 
+  let s = (show f) ++ " and " ++ (show g)
+  if p f g then do
+    print $ "Pass on " ++ s
+    testSetProp2 p (n-1)
+  else error $ "Failed on " ++ s
+
+prop_UnionHasAllElements :: Set Int -> Set Int -> Bool
+prop_UnionHasAllElements f@(Set fs) g@(Set gs) = 
   let u = unionSet f g
   in (all (\x -> inSet x u) fs) && (all (\x -> inSet x u) gs)
+
+prop_UnionHasOnlyElements :: Set Int -> Set Int -> Bool
+prop_UnionHasOnlyElements f@(Set fs) g@(Set gs) = 
+  let (Set us) = unionSet f g
+  in all (\x -> (x `elem` fs) || (x `elem` gs)) us 
+
+-- test using own set generator
+testUnionSet :: Int -> IO ()
+testUnionSet n = testSetProp2 (\f g -> (prop_UnionHasOnlyElements f g) && (prop_UnionHasAllElements f g)) n
 
 intersectionSet :: Ord a => Set a -> Set a -> Set a
 intersectionSet (Set xs) (Set ys) = list2set $ xs `intersect` ys
 
+prop_IntersectionElementsInBoth :: Set Int -> Set Int -> Bool
+prop_IntersectionElementsInBoth f g = 
+  let (Set is) = intersectionSet f g
+  in all (\x -> (inSet x f) && (inSet x g)) is
+
+prop_IntersectionHasAllElements :: Set Int -> Set Int -> Bool
+prop_IntersectionHasAllElements f@(Set fs) g@(Set gs) = 
+  let i = intersectionSet f g
+  in all (\x -> if x `elem` gs then inSet x i else True) fs
+
+testIntersectionSet :: Int -> IO () 
+testIntersectionSet n = testSetProp2 (\f g -> (prop_IntersectionElementsInBoth f g) && (prop_IntersectionHasAllElements f g)) n
+
 differenceSet :: Ord a => Set a -> Set a -> Set a
 differenceSet (Set xs) (Set ys) = list2set $ xs \\ ys
 
+prop_DifferenceElementsNotInOther :: Set Int -> Set Int -> Bool
+prop_DifferenceElementsNotInOther f g = 
+  let (Set ds) = differenceSet f g
+  in all (\x -> not (inSet x g)) ds
+
+prop_DifferenceHasAllElements :: Set Int -> Set Int -> Bool
+prop_DifferenceHasAllElements f@(Set fs) g@(Set gs) = 
+  let d = differenceSet f g
+  in all (\x -> if not (x `elem` gs) then inSet x d else True) fs
+
+testDifferenceSet :: Int -> IO ()
+testDifferenceSet n = testSetProp2 (\f g -> (prop_DifferenceElementsNotInOther f g) && (prop_DifferenceHasAllElements f g)) n
+
+-- END set operations
+-- Time taken 1.5h. 
+-- Tested with own generator and `testUnionSet`, `testIntersectionSet` & `testDifferenceSet`
+-- Tested with QuickCheck using properties `prop_UnionHasAllElements`, `prop_UnionHasOnlyElements`, `prop_IntersectionElementsInBoth`, `prop_IntersectionHasAllElements`, `prop_DifferenceElementsNotInOther` & `prop_DifferenceHasAllElements`
