8d7d89e81e82590fd15dd2c6c634887b38f55471
Gorjan Jovanovski <jovanovski@gorjan.info>
2015-09-21 18:22:26 +0200

    Ex5

---
+++ Private folders/Gorjan/Lab04/Ex5.hs
@@ -0,0 +1,18 @@
+module Ex5 where
+
+import SetOrd
+import Data.List
+import System.Random
+import Test.QuickCheck
+import Control.Monad
+
+type Rel a = [(a,a)]
+
+symClose :: Ord a => Rel a -> Rel a
+symClose [] = []
+symClose ((a,b):xs) = if (b,a) `elem` xs then (a,b) : symClose xs else (a,b) : (b,a) : symClose xs
+
+callSymClose :: Ord a => Rel a -> Rel a
+callSymClose x = nub (symClose x)
+
+-- time spent : 5min
