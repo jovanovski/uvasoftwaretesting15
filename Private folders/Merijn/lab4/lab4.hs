f7a67e2b2106856b1b3b5ebfa88438382959edb4
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-22 18:51:54 +0200

    Added euler 50 solution as extra bonus

--- Private folders/Merijn/lab4/lab4.hs
+++ Private folders/Merijn/lab4/lab4.hs
@@ -7,6 +7,7 @@ import Test.QuickCheck
 import Control.Monad
 import Data.List
 import Data.Char
+import Data.Function (on)
 -- import Data.Function -- using fix function definition from lecture
 import Lecture4
 
@@ -352,3 +353,41 @@ prop_StatementReadShowIsEqual s = s == (read $ show s)
 -- END Bonus - show function
 -- Time taken 6h - this was too much time for a bonus exercise to be fun...
 -- Tested using quickcheck and prop_*ReadShowIsEqual
+
+-- START Extra Bonus - Euler 50
+
+primesbelow :: Integer -> [Integer]
+primesbelow n = sievebelow [2..] where 
+  sievebelow (x:xs) = if x^2 > n then x:takeWhile (< n) xs else x:(sievebelow [y|y <- xs, y `mod` x /= 0])
+
+longestprimeseqsumprimebelow :: Integer -> [Integer]
+longestprimeseqsumprimebelow m = let (c,cl) = helper1 (primesbelow m) [] 0 in c
+  where
+    -- ps = remaining primes
+    -- c = longest sequence
+    -- cl = longest sequence length 
+    helper1 [] c cl = (c,cl)
+    helper1 ps c cl = 
+      let (c2,cl2) = (helper2 ps [] 0 0 c cl)
+      in helper1 (tail ps) c2 cl2
+      where 
+        -- ps = remaining primes
+        -- r = current sequence
+        -- rl = current sequence length
+        -- rs = current sequence sum
+        -- c = longest sequence 
+        -- cl = longest sequence length
+        helper2 [] _ _ _ c cl = (c,cl)
+        helper2 (p:ps) r rl rs c cl = 
+          let rs2 = rs + p
+          in if rs2 >= m then (c, cl) else
+            let r2 = r ++ [p]
+                rl2 = (rl + 1)
+            in if (rl2 > cl) && (rs2 `elem` ps) then helper2 ps r2 rl2 rs2 r2 rl2 else helper2 ps r2 rl2 rs2 c cl
+
+sol50 :: Integer
+sol50 = sum $ longestprimeseqsumprimebelow 1000000
+
+-- END Extra Bonus - Euler 50
+-- Time taken 1h
+
